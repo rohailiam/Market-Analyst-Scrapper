11c43ebb1cd0371854d2038e84c29f8bf58dd439
rohailiam <mrohail2928@gmail.com>
2022-03-31 00:25:50 +0500

    server setup, api completed

---
+++ index.js
@@ -0,0 +1,70 @@
+const puppeteer = require('puppeteer');
+const Cheerio = require('cheerio');
+const express =  require('express');    
+
+const app = express();
+const port = 3000;
+
+
+app.get('/getupcomingnft', async (req, res) => {
+    res.json( await getUpcomingnftData())
+  })
+
+  
+async function getUpcomingnftData(){
+    const url = 'https://upcomingnft.net/most-popular-events/';
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    console.log('tab created');
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    console.log('page fetched');
+    const htmlData = await page.evaluate(() => document.querySelector('*').outerHTML);
+
+    // console.log(htmlData);
+    var $ = Cheerio.load(htmlData);
+    // $('#movietable', htmlData);
+    var formatedData = [];
+    var tableRows = $('#movietable tbody tr');
+    $(tableRows).each((index, element) => {
+        var chldrn = element.children;
+        var childData = {};
+        $(chldrn).each((index, element)=>{
+                // childData[index] = $(element).text().trim();
+            if(index === 2){
+                childData['name'] = $(element).text().trim();
+            }
+            if(index === 5){
+                childData['price'] = $(element).text().trim();
+            }
+            if(index === 6){
+                childData['supply'] = $(element).text().trim();
+            }
+            if(index === 7){
+                childData['twitter-follower'] = $(element).text().trim();
+                childData['twitter-link'] = $(element).find('a').attr('href');
+            }
+            if(index === 8){
+                childData['discord-number'] = $(element).text().trim();
+                childData['discord-link'] = $(element).find('a').attr('href');
+            }
+            if(index === 9){
+                childData['website-link'] = $(element).find('a').attr('href');
+            }
+            // console.dir(element)
+        })
+        formatedData.push(childData);
+    });
+    // console.log(formatedData);
+    return formatedData;
+
+} 
+
+// async function receive(){
+//     var result =  await getUpcomingnftData();
+//     console.log(result);
+// }
+// receive();
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
