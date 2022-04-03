c413ed1464077662d805d8f69d525b87b4de0409
rohailiam <mrohail2928@gmail.com>
2022-04-03 17:27:33 +0500

    removed try catch

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/getscrapeddata', async (req, res) => {
 
 async function getData(){
 
-    try {
+    
     var data = {};
 
     const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
@@ -78,9 +78,6 @@ async function getData(){
 
     return data;
 
-    } catch (error) {
-        return {'msg': 'An error occured, try again'}        
-    }
 }
 
 async function getUpcomingnftData(htmlData){
