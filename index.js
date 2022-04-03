9dd7e2aad17b7762f29cbe4a21ec7df48ca1976d
rohailiam <mrohail2928@gmail.com>
2022-04-03 17:14:05 +0500

    try catch added

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.get('/getscrapeddata', async (req, res) => {
   
 
 async function getData(){
+
+    try {
     var data = {};
 
     const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36';
@@ -75,6 +77,10 @@ async function getData(){
 
 
     return data;
+
+    } catch (error) {
+        return {'msg': 'An error occured, try again'}        
+    }
 }
 
 async function getUpcomingnftData(htmlData){
